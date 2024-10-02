bb400b48afd141612d07f85fac82314d3adf15bb
Carlos Jiménez <iicarlosjim@gmail.com>
2024-10-02 15:55:02 -0600

    chore: Implementación de Mongodb

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 require('dotenv').config()
 
 const server = require('./src/server')
+const db = require('./src/libs/db')
 const PORT = process.env.PORT ?? 8080
 
-server.listen(PORT, () => {
-   console.log(`Server is running on port: ${PORT}`);
-})
+db.connect()
+   .then(result => {
+      server.listen(PORT, () => {
+         console.log(`Server is running on port: ${PORT}`);
+      })
+   })
+   .catch(error => {
+      console.error("DB connection error", error)
+   })
