ae806ef3fd5f2b272bd8443cc2f7588e8ad01e48
Carlos Jiménez <iicarlosjim@gmail.com>
2024-10-03 17:52:20 -0600

    chore: Avance de Stripe

--- src/routes/stripe.js
+++ src/routes/stripe.js
@@ -1,25 +1,79 @@
 const express = require("express");
-const utils = require("../libs/utils");
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY_SANDBOX)
 const createError = require("http-errors");
 const validator = require("../libs/validator");
-const { default: slugify } = require("slugify");
 
 const router = express.Router();
 
-// store
-router.post("/create-customer", async (req, res) => {
+router.get("/", (req, res) => {
+   res.json({message: "Stripe"})
+})
+
+// index
+router.get("/product", async (req, res) => {
+   const products = await stripe.products.list();
+   res.send(products)
+})
+
+// store product
+router.post("/product/create", async (req, res) => {
+   const rules = {
+      name: ["required"]
+   };
+   const validate = validator(rules, req.body);
+
+   if (!validate.validated) {
+      res.status(400)
+      res.send({ status: false, messages: validate.messages });
+      return
+   }
+
+   const products = await stripe.products.search({
+      query: 'name: \'' + req.body.name + '\'', limit: 1
+    });
+   if (products.data.length) {
+      res.send(products.data[0])
+      return
+   }
+
+   const product = await stripe.products.create(req.body)
+
+   const price = await stripe.prices.create({
+      unit_amount: 100,
+      currency: 'usd',
+      recurring: {
+         interval: 'month',
+      },
+      product: product.id
+   })
+   
+   res.send(product)
+})
+
+// store customer
+router.post("/customer/create", async (req, res) => {
+   const rules = {
+      email: ["required"],
+      name: ["required"]
+   };
+   const validate = validator(rules, req.body);
+
+   if (!validate.validated) {
+      res.status(400)
+      res.send({ status: false, messages: validate.messages });
+      return
+   }
    const { email, name } = req.body
    const customer = await stripe.customers.create({
-      email: "test@gmail.com",
-      name: "Test_User"
+      email, name
    }
    )
 
    res.send(customer)
 });
 
-// show
-router.get("/create-subscription", async (req, res) => {
+// store subscription
+router.post("/subscription/create", async (req, res) => {
    const rules = {
       plan: ["required"],
       email: ["required"],
