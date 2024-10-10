0ddc28ddf87e946fee571efd7a5575262aa72b76
Carlos Jiménez <iicarlosjim@gmail.com>
2024-10-10 06:52:14 -0600

    chore: Validaciones en Stripe

--- src/routes/stripe.js
+++ src/routes/stripe.js
@@ -5,7 +5,8 @@ const validator = require("../libs/validator");
 
 const router = express.Router();
 
-router.get('/config', async (req, res) => {
+// stripe configuration
+router.get("/config", async (req, res) => {
    const prices = await stripe.prices.list();
  
    res.send({
@@ -14,11 +15,12 @@ router.get('/config', async (req, res) => {
    });
  });
 
+ // stripe check status
 router.get("/", (req, res) => {
    res.json({message: "Stripe"})
 })
 
-// index
+// index product
 router.get("/product", async (req, res) => {
    const products = await stripe.products.list();
    res.send(products)
@@ -40,29 +42,37 @@ router.post("/product/create", async (req, res) => {
    const products = await stripe.products.search({
       query: 'name: \'' + req.body.name + '\'', limit: 1
     });
-   if (products.data.length) {
-      res.send(products.data[0])
-      return
-   }
 
-   const product = await stripe.products.create(req.body)
+   let product = null
+   if (products.data.length) 
+      product = products.data[0]
+   else 
+      product = await stripe.products.create(req.body)
 
-   const price = await stripe.prices.create({
-      unit_amount: 100,
-      currency: 'usd',
-      recurring: {
-         interval: 'month',
-      },
-      product: product.id
-   })
+   const prices = await stripe.prices.search({
+      query: 'product:\'' + product.id + '\' ',
+   });
+
+   let price = null
+   if (prices.data.length)
+      price = prices.data[0]
+   else
+      price = await stripe.prices.create({
+         unit_amount: 100,
+         currency: 'usd',
+         recurring: {
+            interval: 'month',
+         },
+         product: product.id
+      })
    
-   res.send(product)
+   res.send({product, price})
 })
 
 // store customer
 router.post("/customer/create", async (req, res) => {
    const rules = {
-      email: ["required"]
+      email: ["required", "email"]
    };
    const validate = validator(rules, req.body);
 
@@ -76,35 +86,45 @@ router.post("/customer/create", async (req, res) => {
    // Validar si existe el correo en nuestra base de datos
    // Si no existe, crearlo en stripe
    // Si existe, validar si cuenta con id de stripe
-     // si no cuenta con id, crearlo en stripe 
-   const customer = await stripe.customers.create({email})
+   let customer = null 
+   const customers = await stripe.customers.search({
+      query: 'email:\'' + email + '\' '
+   })
+   
+   // si no cuenta con id, crearlo en stripe 
+   try {
+      if (!customers.data.length)
+         customer = await stripe.customers.create({ email })
+      else 
+         customer = customers.data[0]
 
-   res.send(customer)
+      res.send(customer)
+   } catch (error) {
+      return res.status(400).send({ error: { message: error.message } });
+   }
 });
 
 // store subscription
 router.post("/subscription/create", async (req, res) => {
    const rules = {
-      customerId: ["required"],
+      customer: ["required"],
       price: ["required"]
    };
    const validate = validator(rules, req.body);
 
    if (!validate.validated) {
-      res.status(400)
-      res.send({ status: false, messages: validate.messages });
-      return
+      return res.status(400).send({ status: false, messages: validate.messages });
    }
 
-   const { customerId, price } = req.body
+   const { customer, price } = req.body
 
    // El cliente debe tener un método de pago predeterminado...
    try { 
       const subscription = await stripe.subscriptions.create({
-         customer: customerId,
+         customer,
          items: [{ price }],
-         payment_behavior: 'default_incomplete',
-         expand: ['latest_invoice.payment_intent'],
+         payment_behavior: "default_incomplete",
+         expand: ["latest_invoice.payment_intent"],
       });
    
       res.send({
@@ -116,6 +136,26 @@ router.post("/subscription/create", async (req, res) => {
    catch (error) {
       return res.status(400).send({ error: { message: error.message } });
    }
+})
+
+// index subscriptions
+router.get("/subscriptions", async (req, res) => {
+
+   const subscriptions = {
+      data: {}
+   }
+
+   res.send({ subscriptions })
+})
+
+//Buscar transacciones pendientes del usuario
+router.get("/payments", async (req, res) => {
+
+   const subscriptions = {
+      data: {}
+   }
+
+   res.send({ subscriptions })
 })
 
 module.exports = router;
